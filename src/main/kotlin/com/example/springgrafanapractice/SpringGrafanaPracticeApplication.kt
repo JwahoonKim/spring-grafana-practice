bc94192637a8acbace20955a3b721f9bcd327c17
김좌훈 <kjhoon0330@toss.im>
2024-10-09 02:56:27 +0900

    initial commit

---
+++ src/main/kotlin/com/example/springgrafanapractice/SpringGrafanaPracticeApplication.kt
@@ -0,0 +1,11 @@
+package com.example.springgrafanapractice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringGrafanaPracticeApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringGrafanaPracticeApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/springgrafanapractice/SpringGrafanaPracticeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.springgrafanapractice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringGrafanaPracticeApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
